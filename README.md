de9d02c1d912c88a9447cddd73e131bed5f72323
myAccount <rudaosongfengxu@outlook.com>
2023-02-12 12:42:43 +0800

    project-update-2023-2-12

---
+++ README.md
@@ -0,0 +1,21 @@
+[Next.js](https://nextjs.org/) project bootstrapped with [`create-next-app`](https://github.com/vercel/next.js/tree/canary/packages/create-next-app).
+
+## Getting Started
+
+run the development server:
+
+```bash
+npm run dev
+```
+
+Open [http://localhost:3000/home](http://localhost:3000) with your browser to see the result.
+
+目前已完成主页的布局和样式
+
+from csw 2023.2.12
+
+
+
+
+
+## 
---
+++ src/pages/home/index.js
@@ -0,0 +1,366 @@
+import React from "react";
+import "./index.css";
+
+const Home = () => (
+  <div className="home-page">
+    <div className="header-box">
+      <header className="header">
+        <div className="container">
+          <a className="logo">
+            <img
+              src="https://s2.loli.net/2023/02/11/5hxs2qB1Z4KGPoC.png"
+              alt="金掘土稀"
+              className="logo-img"
+            ></img>
+          </a>
+          <nav className="header-table">
+            <li className="nav-list">
+              <ul className="nav-item">
+                <a className="item-name">首页</a>
+              </ul>
+              <ul className="nav-item">
+                <a className="item-name">沸点</a>
+              </ul>
+              <ul className="nav-item">
+                <a className="item-name">课程</a>
+              </ul>
+              <ul className="nav-item">
+                <a className="item-name">直播</a>
+              </ul>
+              <ul className="nav-item">
+                <a className="item-name">活动</a>
+              </ul>
+              <ul className="nav-item">
+                <a className="item-name">竞赛</a>
+              </ul>
+              <ul className="nav-item">
+                <a className="item-name">商城</a>
+              </ul>
+            </li>
+          </nav>
+        </div>
+        <div className="container">
+          <nav className="view-nav">
+            <div className="view-nav-list">
+              <li className="nav-list">
+                <ul className="nav-item">
+                  <a className="item-name">综合</a>
+                </ul>
+                <ul className="nav-item">
+                  <a className="item-name">关注</a>
+                </ul>
+                <ul className="nav-item">
+                  <a className="item-name">后端</a>
+                </ul>
+                <ul className="nav-item">
+                  <a className="item-name">前端</a>
+                </ul>
+                <ul className="nav-item">
+                  <a className="item-name">Android</a>
+                </ul>
+                <ul className="nav-item">
+                  <a className="item-name">iOS</a>
+                </ul>
+                <ul className="nav-item">
+                  <a className="item-name">人工智能</a>
+                </ul>
+              </li>
+            </div>
+          </nav>
+        </div>
+      </header>
+    </div>
+    <main className="main">
+      <div className="view-timeline">
+        <div className="timeline-container">
+          <div className="timeline-content">
+            <div className="content-container">
+              <header className="content-header">
+                <nav className="header-nav">
+                  <div>
+                    <li className="nav-list">
+                      <ul className="header-nav-item">
+                        <a className="header-item-name">推荐</a>
+                      </ul>
+                      <ul className="header-nav-item">
+                        <a className="header-item-name">最新</a>
+                      </ul>
+                      <ul className="header-nav-item">
+                        <a className="header-item-name">热榜</a>
+                      </ul>
+                    </li>
+                  </div>
+                </nav>
+              </header>
+              <div className="header-list">
+                <li className="header-list-item">
+                  <div className="list-item-box">
+                    <div className="info-container">
+                      <a className="user-message" href="#" target="_blank">
+                        管理员 Rudaosong
+                      </a>
+                      <div className="date">刚刚</div>
+                      <div className="tag-list">不可能实现的事情</div>
+                    </div>
+                    <div className="content-wrapper">
+                      <div className="content-main">
+                        <div className="content-title-row">
+                          <a className="title">
+                            【一次难以置信的经历】Hopeless
+                          </a>
+                        </div>
+                        <div className="content-abstract">
+                          <a href="#" target="_blank">
+                            网络世界啊，真真假假假假真真，恐怕，只有自己的力量值得信赖
+                          </a>
+                        </div>
+                        <ul className="action-list">
+                          <li className="item-view">
+                            <i className="view-control"></i>
+                            <span>3w</span>
+                          </li>
+                          <li className="item-like">
+                            <i className="like-control"></i>
+                            <span>0.5w</span>
+                          </li>
+                          <li className="item-comment">
+                            <i className="comment-control"></i>
+                            <span>0.1w</span>
+                          </li>
+                        </ul>
+                      </div>
+                    </div>
+                  </div>
+                </li>
+                <li className="header-list-item">
+                  <div className="list-item-box">
+                    <div className="info-container">
+                      <a className="user-message" href="#" target="_blank">
+                        管理员 Rudaosong
+                      </a>
+                      <div className="date">刚刚</div>
+                      <div className="tag-list">不可能实现的事情</div>
+                    </div>
+                    <div className="content-wrapper">
+                      <div className="content-main">
+                        <div className="content-title-row">
+                          <a className="title">
+                            【一次难以置信的经历】Hopeless
+                          </a>
+                        </div>
+                        <div className="content-abstract">
+                          <a href="#" target="_blank">
+                            网络世界啊，真真假假假假真真，恐怕，只有自己的力量值得信赖
+                          </a>
+                        </div>
+                        <ul className="action-list">
+                          <li className="item-view">
+                            <i className="view-control"></i>
+                            <span>3w</span>
+                          </li>
+                          <li className="item-like">
+                            <i className="like-control"></i>
+                            <span>0.5w</span>
+                          </li>
+                          <li className="item-comment">
+                            <i className="comment-control"></i>
+                            <span>0.1w</span>
+                          </li>
+                        </ul>
+                      </div>
+                    </div>
+                  </div>
+                </li>
+                <li className="header-list-item">
+                  <div className="list-item-box">
+                    <div className="info-container">
+                      <a className="user-message" href="#" target="_blank">
+                        管理员 Rudaosong
+                      </a>
+                      <div className="date">刚刚</div>
+                      <div className="tag-list">不可能实现的事情</div>
+                    </div>
+                    <div className="content-wrapper">
+                      <div className="content-main">
+                        <div className="content-title-row">
+                          <a className="title">
+                            【一次难以置信的经历】Hopeless
+                          </a>
+                        </div>
+                        <div className="content-abstract">
+                          <a href="#" target="_blank">
+                            网络世界啊，真真假假假假真真，恐怕，只有自己的力量值得信赖
+                          </a>
+                        </div>
+                        <ul className="action-list">
+                          <li className="item-view">
+                            <i className="view-control"></i>
+                            <span>3w</span>
+                          </li>
+                          <li className="item-like">
+                            <i className="like-control"></i>
+                            <span>0.5w</span>
+                          </li>
+                          <li className="item-comment">
+                            <i className="comment-control"></i>
+                            <span>0.1w</span>
+                          </li>
+                        </ul>
+                      </div>
+                    </div>
+                  </div>
+                </li>
+                <li className="header-list-item">
+                  <div className="list-item-box">
+                    <div className="info-container">
+                      <a className="user-message" href="#" target="_blank">
+                        管理员 Rudaosong
+                      </a>
+                      <div className="date">刚刚</div>
+                      <div className="tag-list">不可能实现的事情</div>
+                    </div>
+                    <div className="content-wrapper">
+                      <div className="content-main">
+                        <div className="content-title-row">
+                          <a className="title">
+                            【一次难以置信的经历】Hopeless
+                          </a>
+                        </div>
+                        <div className="content-abstract">
+                          <a href="#" target="_blank">
+                            网络世界啊，真真假假假假真真，恐怕，只有自己的力量值得信赖
+                          </a>
+                        </div>
+                        <ul className="action-list">
+                          <li className="item-view">
+                            <i className="view-control"></i>
+                            <span>3w</span>
+                          </li>
+                          <li className="item-like">
+                            <i className="like-control"></i>
+                            <span>0.5w</span>
+                          </li>
+                          <li className="item-comment">
+                            <i className="comment-control"></i>
+                            <span>0.1w</span>
+                          </li>
+                        </ul>
+                      </div>
+                    </div>
+                  </div>
+                </li>
+              </div>
+            </div>
+          </div>
+          <aside className="index-aside">
+            <div className="sidebar-block">
+              <div className="banner">
+                <a
+                  href="https://wj.toutiao.com/q/217534/1eU421ge/26fa/#/"
+                  target="_blank"
+                >
+                  <img
+                    width="240"
+                    height="200"
+                    src="https://p6-juejin.byteimg.com/tos-cn-i-k3u1fbpfcp/cc4c75e2a9a040c5948838adbdbbcad6~tplv-k3u1fbpfcp-no-mark:480:400:0:0.awebp?"
+                  ></img>
+                </a>
+                <div>
+                  <i className="close-btn"></i>
+                  <a className="label">
+                    {/* <span className="inco">投放</span> */}
+                    <span className="inco">广告</span>
+                  </a>
+                </div>
+              </div>
+            </div>
+            <div className="sidebar-block">
+              <div className="recommend-block">
+                <header className="user-block-header">🎖️作者榜</header>
+                <div className="user-list">
+                  <div className="user">
+                    <a href="#" target="_blank" className="link">
+                      <img
+                        src="https://p3-passport.byteimg.com/img/user-avatar/16beff45c6c05f565b9d0663170a918b~100x100.awebp"
+                        alt="TF男孩"
+                        className="avatar"
+                      ></img>
+                      <div className="user-info">
+                        <a href="#" target="_blank" className="username">
+                          <span className="name">TF男孩</span>
+                          <span className="rank" blank="true">
+                            <img
+                              src="	https://lf3-cdn-tos.bytescm.com/obj/static/xitu_juejin_web/img/lv-5.d08789d.png"
+                              alt=""
+                              title="lv"
+                            ></img>
+                          </span>
+                        </a>
+                        <div className="position">
+                          编程表演艺术家 @ 教育行业-人工智能
+                        </div>
+                      </div>
+                    </a>
+                  </div>
+                  <div className="user">
+                    <a href="#" target="_blank" className="link">
+                      <img
+                        src="https://p3-passport.byteimg.com/img/user-avatar/16beff45c6c05f565b9d0663170a918b~100x100.awebp"
+                        alt="TF男孩"
+                        className="avatar"
+                      ></img>
+                      <div className="user-info">
+                        <a href="#" target="_blank" className="username">
+                          <span className="name">TF男孩</span>
+                          <span className="rank" blank="true">
+                            <img
+                              src="	https://lf3-cdn-tos.bytescm.com/obj/static/xitu_juejin_web/img/lv-5.d08789d.png"
+                              alt=""
+                              title="lv"
+                            ></img>
+                          </span>
+                        </a>
+                        <div className="position">
+                          编程表演艺术家 @ 教育行业-人工智能
+                        </div>
+                      </div>
+                    </a>
+                  </div>
+                  <div className="user">
+                    <a href="#" target="_blank" className="link">
+                      <img
+                        src="https://p3-passport.byteimg.com/img/user-avatar/16beff45c6c05f565b9d0663170a918b~100x100.awebp"
+                        alt="TF男孩"
+                        className="avatar"
+                      ></img>
+                      <div className="user-info">
+                        <a href="#" target="_blank" className="username">
+                          <span className="name">TF男孩</span>
+                          <span className="rank" blank="true">
+                            <img
+                              src="	https://lf3-cdn-tos.bytescm.com/obj/static/xitu_juejin_web/img/lv-5.d08789d.png"
+                              alt=""
+                              title="lv"
+                            ></img>
+                          </span>
+                        </a>
+                        <div className="position">
+                          编程表演艺术家 @ 教育行业-人工智能
+                        </div>
+                      </div>
+                    </a>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </aside>
+        </div>
+      </div>
+    </main>
+    <footer className="footer">
+      <h3 className="footer-title">Footer</h3>
+    </footer>
+  </div>
+);
+
+export default Home;
